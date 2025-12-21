29c8896facaa86ed15a618cd478f8389a98078b6
Takashi SASAKI <takashi316@gmail.com>
2025-12-21 21:15:23 +0000

    feat: Add firebase configuration

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD4rGrdjoS1DAh3B9HdgHWEmWs7IQqblO4",
+  authDomain: "moukaeritaid.firebaseapp.com",
+  databaseURL: "https://moukaeritaid-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "moukaeritaid",
+  storageBucket: "moukaeritaid.firebasestorage.app",
+  messagingSenderId: "1042140630327",
+  appId: "1:1042140630327:web:319d0bba4defb71f6b9eb2"
+};
